d7baa2d6e54abaedcc172c02c680ecbf39057ce1
Eugene Korpan <eugene@svitla.com>
2013-03-18 13:56:59 +0200

    extracted code related to tenant depended model into a separate method for further reuse

--- lib/multitenant-mysql/active_record_extension.rb
+++ lib/multitenant-mysql/active_record_extension.rb
@@ -1,10 +1,30 @@
 class ActiveRecord::Base
-  def self.acts_as_tenant
+  def self.acts_as_tenants_bucket
     after_create do
       config = Rails.configuration.database_configuration
       password = config[Rails.env]["password"]
       ActiveRecord::Base.connection.execute "GRANT ALL PRIVILEGES ON *.* TO '#{self.name}'@'localhost' IDENTIFIED BY '#{password}' WITH GRANT OPTION;"
       ActiveRecord::Base.connection.execute "flush privileges;"
+    end
+  end
+
+  def self.acts_as_tenant
+    view_name = model_name.to_s.downcase.pluralize + "_view"
+    # check whether the view exists in db
+    if ActiveRecord::Base.connection.table_exists? view_name
+      self.class_eval do
+        cattr_accessor :original_table_name
+
+        self.original_table_name = self.table_name
+        self.table_name = view_name
+        self.primary_key = :id
+
+        def self.new(*args)
+          object = super(*args)
+          object.id = nil # workaround for https://github.com/rails/rails/issues/5982
+          object
+        end
+      end
     end
   end
 
@@ -14,28 +34,11 @@ class ActiveRecord::Base
 
     model_name = child.to_s
     if Multitenant::Mysql.models.include? model_name
-      view_name = model_name.to_s.downcase.pluralize + "_view"
-
-      # check whether the view exists in db
-      if ActiveRecord::Base.connection.table_exists? view_name
-        child.class_eval do
-          cattr_accessor :original_table_name
-
-          self.original_table_name = self.table_name
-          self.table_name = view_name
-          self.primary_key = :id
-
-          def self.new(*args)
-            object = super(*args)
-            object.id = nil # workaround for https://github.com/rails/rails/issues/5982
-            object
-          end
-        end
-      end
+      child.send :acts_as_tenant
     end
 
     if Multitenant::Mysql.tenant == child
-      child.send :acts_as_tenant
+      child.send :acts_as_tenants_bucket
     end
   end
 end
