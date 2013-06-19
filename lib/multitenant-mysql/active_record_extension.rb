2dc07d98da3c27b2a188c6b6a4985af170b6e741
eugene <eugene@eugene-pc.(none)>
2013-06-20 00:53:35 +0300

    Fixed issue https://github.com/eugenekorpan/multitenant-mysql/issues/28
    - changed patching of `ActiveRecord::Base.inherited` to alias chaining

--- lib/multitenant-mysql/active_record_extension.rb
+++ lib/multitenant-mysql/active_record_extension.rb
@@ -1,45 +1,52 @@
-class ActiveRecord::Base
-  def self.acts_as_tenants_bucket
-    after_create do
-      password = Multitenant::Mysql::DB.configs['password']
-      tenant_name = self.send(Multitenant::Mysql.configs.bucket_field)
-      connection = Multitenant::Mysql::DB.connection
-      connection.execute "GRANT ALL PRIVILEGES ON *.* TO '#{tenant_name}'@'localhost' IDENTIFIED BY '#{password}' WITH GRANT OPTION;"
-      connection.execute "flush privileges;"
+ActiveRecord::Base.class_eval do
+
+  class << self
+
+    def acts_as_tenants_bucket
+      after_create do
+        password = Multitenant::Mysql::DB.configs['password']
+        tenant_name = self.send(Multitenant::Mysql.configs.bucket_field)
+        connection = Multitenant::Mysql::DB.connection
+        connection.execute "GRANT ALL PRIVILEGES ON *.* TO '#{tenant_name}'@'localhost' IDENTIFIED BY '#{password}' WITH GRANT OPTION;"
+        connection.execute "flush privileges;"
+      end
     end
-  end
 
-  def self.acts_as_tenant
-    view_name = model_name.to_s.downcase.pluralize + "_view"
-    # check whether the view exists in db
-    if ActiveRecord::Base.connection.table_exists? view_name
-      self.class_eval do
-        cattr_accessor :original_table_name
+    def acts_as_tenant
+      view_name = model_name.to_s.downcase.pluralize + "_view"
+      # check whether the view exists in db
+      if ActiveRecord::Base.connection.table_exists? view_name
+        self.class_eval do
+          cattr_accessor :original_table_name
 
-        self.original_table_name = self.table_name
-        self.table_name = view_name
-        self.primary_key = :id
+          self.original_table_name = self.table_name
+          self.table_name = view_name
+          self.primary_key = :id
 
-        def self.new(*args)
-          object = super(*args)
-          object.id = nil # workaround for https://github.com/rails/rails/issues/5982
-          object
+          def self.new(*args)
+            object = super(*args)
+            object.id = nil # workaround for https://github.com/rails/rails/issues/5982
+            object
+          end
         end
       end
     end
-  end
 
-  def self.inherited(child)
-    super
+    alias_method :multitenant_mysql_chained_inherited, :inherited
 
-    model_name = child.to_s
-    if Multitenant::Mysql.configs.models.include? model_name
-      child.send :acts_as_tenant
+    def inherited(child)
+      multitenant_mysql_chained_inherited(child)
+
+      model_name = child.to_s
+      if Multitenant::Mysql.configs.models.include? model_name
+        child.send :acts_as_tenant
+      end
+
+      if Multitenant::Mysql.configs.tenant? child
+        child.send :acts_as_tenants_bucket
+      end
     end
 
-    if Multitenant::Mysql.configs.tenant? child
-      child.send :acts_as_tenants_bucket
-    end
-
   end
+
 end
